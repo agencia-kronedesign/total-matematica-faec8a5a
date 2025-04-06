d305cc5734f931d2504d2ee1210f01c0a95151fc
Luiz Tibiriça <comercial@kronedesign.com.br>
2025-04-06 16:04:07 -0300

    troca de logotipo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,14 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Logo: React.FC = () => {
   return (
     <Link to="/" className="inline-block">
-      <h1 className="text-xl md:text-2xl font-bold text-totalBlue">
-        <span className="text-totalYellow">T</span>
-        <span>O</span>
-        <span className="text-totalYellow">T</span>
-        <span>AL</span>
-      </h1>
+      <img 
+        src="/lovable-uploads/logo.png" 
+        alt="Total Matemática" 
+        className="h-10 md:h-12" 
+      />
     </Link>
   );
 };
